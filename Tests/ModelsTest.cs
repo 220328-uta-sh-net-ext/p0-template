1edbba99b955d56ddae47c5932efdeefa9904bf2
Juniper Song <songminseonk@gmail.com>
2022-04-22 15:36:34 -0700

    Update ModelsTest.cs

--- Tests/ModelsTest.cs
+++ Tests/ModelsTest.cs
@@ -1,22 +1,14 @@
 using Xunit;
 using Models;
 
-//Make sure you have your projects and namespaces you need here
 namespace Tests;
 
 public class ModelsTest
 {
     [Fact]
-    public void RestaurantShouldCreate()
+    public void UnitTest1()
     {
-        //Arrange
-        //In this case, the arrange step, was making sure that I had my Models namespace included here.
 
-        //Act: Creating the restaurant object
-        Restaurant testRestaurant = new Restaurant();
-
-        //Assert: assert that my testRestaurant actually created and is not null
-        Assert.NotNull(testRestaurant);
     }
 
 }
