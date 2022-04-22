295f165f32d0432fc63f05c5c6aaab290a682dad
Juniper Song <songminseonk@gmail.com>
2022-04-22 17:53:14 -0400

    template

---
+++ DL/FileRepo.cs
@@ -0,0 +1,37 @@
+using System.Text.Json;
+
+namespace DL;
+
+//This class reads and writes to the file
+public class FileRepo : IRepo
+{
+    public FileRepo()
+    { }
+
+    private string filePath = "../DL/Restaurants.json";
+
+    public List<Restaurant> GetAllRestaurants()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void AddRestaurant(Restaurant restaurantToAdd)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void AddReview(int restaurantId, Review reviewToAdd)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Restaurant> SearchRestaurants(string searchTerm)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool IsDuplicate(Restaurant restaurant)
+    {
+        throw new NotImplementedException();
+    }
+}
